de1fdd3b9ec195cc7f78d743d0f093fb450327f9
Pythonsway <adam@pythonsway.it>
2019-08-18 13:40:03 +0200

    Updates

---
+++ precache-manifest.05d622678daa63afd54d8d6c959bdd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346eaebc0471bea35cb837386ac99edd",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "551abe32a6264e2b9af4",
+    "url": "/pomodoro-clock/static/css/main.e5ddc188.chunk.css"
+  },
+  {
+    "revision": "d65217de4026b3ae8a02",
+    "url": "/pomodoro-clock/static/js/2.eda1f790.chunk.js"
+  },
+  {
+    "revision": "551abe32a6264e2b9af4",
+    "url": "/pomodoro-clock/static/js/main.b2b101f2.chunk.js"
+  },
+  {
+    "revision": "a94c9c237e61dbb35ae8",
+    "url": "/pomodoro-clock/static/js/runtime~main.b8c7466b.js"
+  },
+  {
+    "revision": "1387bde847215a75b331a03db88c6180",
+    "url": "/pomodoro-clock/static/media/alarm.1387bde8.mp3"
+  }
+]);
